255f1132c2461722887ccb9e5f77a316575f6eaf
mahmoudshalaby <75578092+mahmoud709@users.noreply.github.com>
2023-09-04 18:06:38 +0300

    handle unknown routes

--- server/app.js
+++ server/app.js
@@ -13,6 +13,9 @@ const app=express();
 app.use(cors())
 app.use(express.json())
 app.use('/',route)
+app.use('*',(req,res)=>{
+    res.send('<h3>Welcome to server</h3>')
+})
 app.listen(process.env.PORT|5000,(req,res)=>{
     console.log(`App is Running`)
 })
